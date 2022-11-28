2e4dcb52622e464333111a379f58e7c9ff902dd0
ANILmarry <anilubbana@gmail.com>
2022-11-28 11:31:46 +0530

    first commit

---
+++ force-app/main/default/lwc/firstcomp/firstcomp.js
@@ -0,0 +1,25 @@
+import { LightningElement } from 'lwc';
+
+export default class Firstcomp extends LightningElement {
+    value = 'inProgress';
+
+    get options() {
+        return [
+            { label: 'New', value: 'new' },
+            { label: 'In Progress', value: 'inProgress' },
+            { label: 'Finished', value: 'finished' },
+        ];
+    }
+
+    handleChange(event) {
+        this.value = event.detail.value;
+    }
+    queryTerm;
+
+    handleKeyUp(evt) {
+        const isEnterKey = evt.keyCode === 13;
+        if (isEnterKey) {
+            this.queryTerm = evt.target.value;
+        }
+    }
+}
